96f3957d79c81c766340e7ee6f8ac6ea8269c6c5
Flor77 <budaflorin@yahoo.com>
2023-05-05 18:51:31 +0300

    checkbox modified

--- src/components/todo-item/TodoItem.jsx
+++ src/components/todo-item/TodoItem.jsx
@@ -1,41 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TodoItem.css";
 import Checkbox from "../checkbox/CheckBox";
 
 const TodoItem = (props) => {
-
   const handleCheckboxChange = () => {
-    const matchId = props.id
-    props.completeTodo(matchId)
+    props.completeTodo(props.id);
   };
 
   const handleDeleteChange = () => {
-    const matchId = props.id
-    props.removeTodo(matchId)
-  }; 
-
-  const handleEditId = () => {
-    const matchId = props.id
-    props.onEdit(matchId)
+    props.removeTodo(props.id);
   };
 
-  
+  const handleEditId = () => {
+    props.onEdit(props.id);
+  };
 
   return (
     <div className={`todo-item ${props.completed && "todo-completed"}`}>
       <div className="todo-item-header">
         <div className="title-area">
-          <Checkbox
-            checked={!!props.completed}
-            onChange={handleCheckboxChange}
-          />
+          <Checkbox checked={props.completed} onChange={handleCheckboxChange} />
 
           <h4>{props.title}</h4>
-          
         </div>
         <div>
-          <i  onClick={handleEditId} className="fa fa-pencil" aria-hidden="true"></i>
-          <i onClick={handleDeleteChange} className="fa fa-trash" aria-hidden="true"></i>
+          <i
+            onClick={handleEditId}
+            className="fa fa-pencil"
+            aria-hidden="true"
+          ></i>
+          <i
+            onClick={handleDeleteChange}
+            className="fa fa-trash"
+            aria-hidden="true"
+          ></i>
         </div>
       </div>
 
